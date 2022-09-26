79c2a8a934d37d6ed3d20b39fb0d9e0135b077e6
Jesus Uzcategui <uzcateguijesusdev@gmail.com>
2022-09-25 22:05:03 -0500

    init header

---
+++ webpack.common.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+const JS_DIR = path.resolve(__dirname, './src');
+
+const entry = {
+  theme: './src/templates/index.js',
+  //product: './src/templates/product.js',
+  //password: './src/templates/password.js'
+}
+
+const output = {
+  filename: '[name].js',
+  path: path.resolve(__dirname, 'assets')
+};
+
+const rules = [
+  {
+    test: /\.js$/,
+    include: [JS_DIR],
+    exclude: /node_modules/,
+    use: 'babel-loader'
+  }
+];
+
+module.exports = {
+
+  entry: entry,
+
+  output: output,
+
+  module: {
+    rules: rules,
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin(),
+      new CssMinimizerPlugin(),
+    ]
+  },
+
+}
